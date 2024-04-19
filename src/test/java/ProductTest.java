fe62499bb5979e05fe5dfd5c8ebcb36850801733
Fedor Danilenko <divine.world.mail@gmail.com>
2024-04-19 17:49:36 +0400

    test

--- src/test/java/ProductTest.java
+++ src/test/java/ProductTest.java
@@ -80,35 +80,35 @@ public class ProductTest {
 
     @Test
     public void partTest() {
-        Product house = new Product("house");
-        Product door = new Product("door");
-        Product window = new Product("window");
-        Product walls = new Product("walls");
-        Product woods = new Product("woods");
-        Product knob = new Product("knob");
-        Product glass = new Product("glass");
-        Product brick = new Product("brick");
-        Product cement = new Product("cement");
+        Product dough = new Product("dough");
+        Product flour = new Product("flour");
+        Product eggs = new Product("eggs");
+        Product water = new Product("water");
+        Product wheat = new Product("wheat");
 
-        assertTrue(door.addProduct(woods));
-        assertTrue(door.addProduct(knob));
+        assertTrue(wheat.addProduct(dough));
+        assertTrue(flour.addProduct(wheat));
 
-        assertTrue(window.addProduct(woods));
-        assertTrue(window.addProduct(glass));
-        assertTrue(window.addProduct(knob));
+        assertTrue(dough.addProduct(eggs));
+        assertTrue(dough.addProduct(water));
 
-        assertTrue(walls.addProduct(brick));
-        assertTrue(walls.addProduct(cement));
+        assertFalse(dough.addProduct(flour));
+    }
 
-        assertTrue(house.addProduct(walls));
-        assertTrue(house.addProduct(door));
-        assertTrue(house.addProduct(window));
+    @Test
+    public void partTest2() {
+        Product dough = new Product("dough");
+        Product flour = new Product("flour");
+        Product eggs = new Product("eggs");
+        Product water = new Product("water");
+        Product wheat = new Product("wheat");
 
-        assertFalse(walls.addProduct(house));
-        assertFalse(woods.addProduct(house));
-        assertFalse(walls.addProduct(house));
-        assertFalse(knob.addProduct(door));
-        assertFalse(knob.addProduct(window));
+        assertTrue(flour.addProduct(wheat));
+        assertTrue(wheat.addProduct(dough));
 
+        assertTrue(dough.addProduct(eggs));
+        assertTrue(dough.addProduct(water));
+
+        assertTrue(dough.addProduct(flour));
     }
 }
