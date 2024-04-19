2749b741a0afc0a2d32995375e5bb109d3985659
Fedor Danilenko <divine.world.mail@gmail.com>
2024-04-19 13:45:18 +0400

    string

--- src/main/java/org/example/Product.java
+++ src/main/java/org/example/Product.java
@@ -5,30 +5,34 @@ import java.util.Set;
 
 public class Product {
     private String name;
-    private Set<Product> ingredients;
+    private Set<String> ingredients;
 
     public Product(String name) {
         this.name = name;
         this.ingredients = new HashSet<>();
     }
 
-    private Set<Product> getIngredients() {
+    private Set<String> getIngredients() {
         return ingredients;
+    }
+
+    private String getName() {
+        return name;
     }
 
     public boolean addProduct(Product product) {
         if (product.hasIngredient(this)) {
             return false;
         }
-        ingredients.add(product);
+        ingredients.add(product.getName());
         ingredients.addAll(product.getIngredients());
         return true;
     }
 
     private boolean hasIngredient(Product product) {
-        if (this == product) {
+        if (this.getName().equals(product.getName())) {
             return true;
         }
-        return this.getIngredients().contains(product);
+        return this.getIngredients().contains(product.getName());
     }
 }
